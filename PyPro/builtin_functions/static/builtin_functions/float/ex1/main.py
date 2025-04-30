24fa4b5cab08ab5f841566f8e312f26c4e33e150
DimRv <120788544+DimRv@users.noreply.github.com>
2025-04-30 16:23:54 +0300

    add float function

---
+++ PyPro/builtin_functions/static/builtin_functions/float/ex1/main.py
@@ -0,0 +1,6 @@
+"""Пример использования встроенной функции float"""
+
+num1 = float('-3.53')
+num2 = float(1)
+print(num1, type(num1))  # -3.53 &lt;class 'float'>
+print(num2, type(num2))  # 1.0 &lt;class 'float'>
