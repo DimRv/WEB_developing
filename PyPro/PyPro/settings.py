03dde065ed0e46b66a4c421de31057fd4e9959ed
DimRv <120788544+DimRv@users.noreply.github.com>
2025-05-03 22:06:14 +0300

    add startapp builtins

--- PyPro/PyPro/settings.py
+++ PyPro/PyPro/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'base',
     'builtin_functions',
+    'builtin',
 ]
 
 MIDDLEWARE = [
@@ -75,10 +76,19 @@ WSGI_APPLICATION = 'PyPro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}"""
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "my_service",
+        },
     }
 }
 
--- PyPro/test.py
+++ PyPro/test.py
@@ -1,1 +1,1 @@
-from django.core.management import execute_from_command_line
+import psycopg
